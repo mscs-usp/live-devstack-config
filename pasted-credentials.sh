86e54d091833a586fe26895f91022d17e0bf5f64
Albert Vonpupp <vonpupp@gmail.com>
2013-09-13 18:04:42 -0300

    Added localrc files and credentials

---
+++ pasted-credentials.sh
@@ -0,0 +1,6 @@
+export SERVICE_TOKEN=password
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=password
+export OS_AUTH_URL=http://localhost:5000/v2.0/
+export SERVICE_ENDPOINT=http://localhost:35357/v2.0
