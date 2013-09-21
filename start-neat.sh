49105240ca8b214e711d46d48350fd790e0131a3
Albert De La Fuente <vonpupp@gmail.com>
2013-09-21 19:24:52 -0300

    Added start-neat.sh

---
+++ start-neat.sh
@@ -0,0 +1,33 @@
+# REQUIREMENTS
+
+# NFS:
+sudo service rpcbind start
+sudo mkdir -p /var/lib/nova/instances
+sudo mount -o hard,intr,noatime 10.7.22.10:/nova-instances /var/lib/nova/instances
+
+# ----
+
+# 1. Clone/pull OpenStack-neat
+cd /opt/stack/openstack-neat/; git pull
+
+# 2. Install dependencies
+sudo /opt/stack/openstack-neat/setup/deps-fedora-devstack.sh
+sudo /opt/stack/openstack-neat/setup/deps-fedora-devstack-nfs.sh
+
+# 3. Edit neat.conf
+
+# 4. Install OpenStack-Neat
+sudo python /opt/stack/openstack-neat/setup.py install
+
+# 5.
+sudo /opt/stack/openstack-neat/compute-clone-neat.py
+
+# 6.
+sudo /opt/stack/openstack-neat/compute-install-deps.py
+sudo /opt/stack/openstack-neat/compute-install-neat.py
+
+# 7.
+sudo /opt/stack/openstack-neat/compute-copy-conf.py
+
+# 8.
+sudo /opt/stack/openstack-neat/all-start.sh
